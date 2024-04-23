db499d65b067ee62e5241cb5df13978a14e08c4b
Regla Hermosín Rodríguez <rhermosinrodriguez@gmail.com>
2024-04-23 18:29:51 +0200

    Start cloudinary

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import userRouter from './routes/user.routes';
 import cors from 'cors'
+import fileUpload from "express-fileupload"
+import songRouter from "./routes/song.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/user", userRouter )
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./upload"
+}));
+app.use("/user", userRouter)
+app.use("/song", songRouter)
 
 export default app;

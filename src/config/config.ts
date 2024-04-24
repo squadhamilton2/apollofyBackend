b052a50ceeeeb343cca88849af94d4c249480c2a
Regla Hermosín Rodríguez <rhermosinrodriguez@gmail.com>
2024-04-24 13:44:16 +0200

    Install multer

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,13 @@ type TConfig = {
 type EnvironmentConfig = {
   app: AppConfig;
   db: DbConfig;
+  cloudinary: CloudinaryConfig;
+};
+
+type CloudinaryConfig = {
+  CLOUD_NAME: string;
+  API_KEY: string;
+  API_SECRET: string;
 };
 
 type AppConfig = {
@@ -25,23 +32,42 @@ if (process.env.NODE_ENV === "production") {
 
 const ENV = process.env.NODE_ENV ?? "development";
 
+const {
+  PORT,
+  MONGODB_URI,
+  CLOUDINARY_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_SECRET,
+} = process.env;
+
 const CONFIG: TConfig = {
   development: {
-    app: {
-      PORT: process.env.PORT || 4001,
-    },
-    db: {
-      URI: process.env.MONGODB_URI || "mongodb://localhost:27017",
-    },
+      app: {
+          PORT: PORT || 4001,
+      },
+      db: {
+          URI: MONGODB_URI || "mongodb://localhost:27017",
+      },
+      cloudinary: {
+          CLOUD_NAME: CLOUDINARY_NAME || "cloud_name",
+          API_KEY: CLOUDINARY_API_KEY || "api_key",
+          API_SECRET: CLOUDINARY_SECRET || "api_secret",
+      },
   },
   production: {
-    app: {
-      PORT: process.env.PORT || 8081,
-    },
-    db: {
-      URI: process.env.MONGODB_URI || "mongodb://localhost:27017",
-    },
+      app: {
+          PORT: process.env.PORT || 8081,
+      },
+      db: {
+          URI: process.env.MONDOBD_URI || "mongodb://localhost:27017",
+      },
+      cloudinary: {
+          CLOUD_NAME: CLOUDINARY_NAME || "cloud_name",
+          API_KEY: CLOUDINARY_API_KEY || "api_key",
+          API_SECRET: CLOUDINARY_SECRET || "api_secret",
+      },
   },
 };
+
 
 export default CONFIG[ENV];
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import express from "express";
 import userRouter from './routes/user.routes';
 import cors from 'cors'
-import fileUpload from "express-fileupload"
 import songRouter from "./routes/song.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(fileUpload({
-    useTempFiles: true,
-    tempFileDir: "./upload"
-}));
 app.use("/user", userRouter)
 app.use("/song", songRouter)
 

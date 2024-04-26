51b0ca0886c685b94c43b8875d1ea2bf4a9b00c6
Earnen <p.wilmun@gmail.com>
2024-04-26 12:53:46 +0200

    test createSong

--- src/routes/song.routes.ts
+++ src/routes/song.routes.ts
@@ -1,17 +1,20 @@
 import { Router } from "express";
 import {
-    createSong,
-    deleteSong,
-    getAllSong,
-    // updateSong,
-
+  createSong,
+  deleteSong,
+  getAllSong,
+  // updateSong,
 } from "../controllers/song.controllers";
-
+import {
+  multerCloudinarySong,
+  multerCloudinaryImage,
+} from "../utils/multer-cloudinary";
+console.log({ multerCloudinaryImage });
 
 const songRouter = Router();
 
 songRouter.get("/", getAllSong);
-songRouter.post("/", createSong);
+songRouter.post("/:userId", multerCloudinaryImage.single("image"), createSong);
 // songRouter.patch("/:songId", updateSong);
 songRouter.delete("/:songId", deleteSong);
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from "express";
-import userRouter from './routes/user.routes';
-import cors from 'cors'
+import userRouter from "./routes/user.routes";
+// import cors from "cors";
 import songRouter from "./routes/song.routes";
+// import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use("/user", userRouter)
-app.use("/song", songRouter)
+// app.use(bodyParser.json());
+// app.use(cors());
+app.use("/user", userRouter);
+app.use("/song", songRouter);
 
 export default app;
